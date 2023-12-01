7b4bc665b3f66ea69e672b5400d065eea998aa30
Abdul Aziz <73788986+abdaziz-04@users.noreply.github.com>
2023-12-01 18:00:14 +0700

    Update home.php

--- app/views/admin/home.php
+++ app/views/admin/home.php
@@ -5,21 +5,67 @@
   <title>Home</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <!-- <link rel="stylesheet" href="css/style.css"> -->
   
   <style>
     * {
       font-family: Montserrat;
+      background: #EBEFF5;
+    }
+
+    .content {
+      padding: 14px 42px 14px 42px;
     }
     .content h2 {
       font-size: 40px;
       color: #E7AE0E;
     }
+
+    .content h3 {
+      font-size: 30px;
+      color: #E7AE0E;
+    }
+
+    .content p {
+      color: #3C8DBB;
+    }
+
+    /* Search */
+    .search button{
+      top: 5px;
+      right: 5px;
+      height: 54px;
+      width: 160px;
+      background: #3C8DBB;
+    }
+
+    .search{
+      display: flex;
+      align-items: center;
+    }
+
+    .search input{
+    height: 54px;
+    width: 734;
+    border-radius: 5px;
+    border: 3px solid #3C8DBB;
+    display: flex;
+    align-items: center;
+    margin-right: 10px;
+    }
   </style>
 </head>
 <body>
-<div class="content" style="margin-left: 250px; padding: 20px;">
+<div class="content">
+  <h3>Beranda</h3>
   <h2>Selamat Datang</h2>
+  <p>Berikut adalah peminjaman barang yang diajukan di website</p>
+      <div class="search">
+        <input type="text" class="form-control">
+        <button class="btn btn-primary">
+        Search <i class="fa fa-search"></i> 
+        <img src="asset/asdasdasda.svg" alt="">
+        </button>
+      </div>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
