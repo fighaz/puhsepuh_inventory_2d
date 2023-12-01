647637e2244bd4f082b614c973cc55c7d208f21a
Abdul Aziz <73788986+abdaziz-04@users.noreply.github.com>
2023-12-01 13:32:58 +0700

    pindah file

--- app/views/admin/home.php
+++ app/views/admin/home.php
@@ -5,6 +5,7 @@
   <title>Home</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="stylesheet" href="css/style.css">
   
   <style>
     .sidebar {
