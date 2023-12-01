fb6329c885ae0cd8fb7055e6a84c53300c30795a
Abdul Aziz <73788986+abdaziz-04@users.noreply.github.com>
2023-12-01 13:23:11 +0700

    Update home.php

--- app/views/admin/home/home.php
+++ app/views/admin/home/home.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Sidebar dengan Bootstrap</title>
+  <title>Home</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   
