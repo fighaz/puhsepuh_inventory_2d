fbf38ab0c15dff3695ac4fae0f34e59c75f9e849
Abdul Aziz <73788986+abdaziz-04@users.noreply.github.com>
2023-12-01 15:19:17 +0700

    create page admin

---
+++ app/views/admin/home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Home</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!-- <link rel="stylesheet" href="css/style.css"> -->
+  
+  <style>
+    * {
+      font-family: Montserrat;
+    }
+    .content h2 {
+      font-size: 40px;
+      color: #E7AE0E;
+    }
+  </style>
+</head>
+<body>
+<div class="content" style="margin-left: 250px; padding: 20px;">
+  <h2>Selamat Datang</h2>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
