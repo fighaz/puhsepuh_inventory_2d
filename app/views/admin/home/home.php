ae4c5910ecb2e18807c64cc4f077d83dbce7c733
Abdul Aziz <73788986+abdaziz-04@users.noreply.github.com>
2023-12-01 12:56:38 +0700

    sidebar

---
+++ app/views/admin/home/home.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Sidebar dengan Bootstrap</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  
+  <style>
+    .sidebar {
+      position: fixed;
+      top: 0;
+      left: 0;
+      height: 100%;
+      width: 250px;
+      background-color: #222D30;;
+      padding-top: 20px;
+      color: white;
+      transition: all 0.3s ease; 
+    }
+    .sidebar a {
+      padding: 10px;
+      text-decoration: none;
+      color: white;
+      display: flex;
+      align-items: center;
+      justify-content: space-between; 
+    }
+    .sidebar a:hover {
+        background-color: #E7AE0E;
+    }
+    .sidebar .icon {
+      margin-left: 25px;
+      margin-right: 38px; 
+    }
+  </style>
+</head>
+<body>
+
+
+<div class="sidebar" id="sidebar">
+  <a href="#">
+    <i class="material-icons">menu</i>
+  </a>
+  <a href="#">
+    Home <i class="material-icons icon">home</i>
+  </a>
+  <a href="#">
+    Inventaris <i class="material-icons icon">inventory</i>
+  </a>
+  <a href="#">
+    Peminjaman <i class="material-icons icon">note_add</i>
+  </a>
+  <a href="#">
+    Ganti Password <i class="material-icons icon">lock</i>
+  </a>
+  <a href="#">
+    Keluar <i class="material-icons icon">exit_to_app</i>
+  </a>
+</div>
+
+<div class="content" style="margin-left: 250px; padding: 20px;">
+  <h2>Isi Konten Halaman</h2>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
