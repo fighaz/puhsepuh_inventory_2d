f6428864b6fef65fdc18ad269ccb044a07b40448
Abdul Aziz <73788986+abdaziz-04@users.noreply.github.com>
2023-12-05 11:44:28 +0700

    Update searchbar.php

--- app/views/templates/searchbar.php
+++ app/views/templates/searchbar.php
@@ -5,3 +5,30 @@
         <img src="asset/asdasdasda.svg" alt="">
         </button>
 </div>
+
+<!-- Style -->
+<style>
+     /* Search */
+     .search button{
+      top: 5px;
+      right: 5px;
+      height: 54px;
+      width: 160px;
+      background: #3C8DBB;
+    }
+
+    .search{
+      display: flex;
+      align-items: center;
+    }
+
+    .search input{
+    height: 54px;
+    width: 734;
+    border-radius: 5px;
+    border: 3px solid #3C8DBB;
+    display: flex;
+    align-items: center;
+    margin-right: 10px;
+    }
+</style>
