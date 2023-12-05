e8f9565c15af3ad9ebe3ece1d3f0dabe901d9388
MohammadAlimul <mohammad4limul@gmail.com>
2023-12-05 13:08:45 +0700

    Pecah header dan footer menjadi template

--- app/views/user/index.php
+++ app/views/user/index.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <title></title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-    <!--<script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI/tZ1ZqjKw0BOyL8GfZ2mPAmUw/A763lSNtFqIo=" crossorigin="anonymous"></script>-->
-    <script src="js/jquery.js"></script>
-</head>
-
-<body>
     <div class="container page d-flex flex-column">
         <div id="welcome" class="title">
             SELAMAT DATANG
@@ -374,6 +360,3 @@
             background-color: red;
         }
     </style>
-</body>
-
-</html>
