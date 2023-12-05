b41ae85292f0a09dc449d574d317a6019d213dcb
fighaz <sofisugiartozaini@gmail.com>
2023-12-05 14:16:19 +0700

    fix

--- public/js/script.js
+++
@@ -1,7 +0,0 @@
-$(".tombolTambahDataPeminjam").on("click", function () {
-    $("#formModalPeminjamLabel").html("Tambah Data peminjam");
-    $(".modal-footer button[type=submit]").html("Tambah Data");
-    $("#username").val("");
-    $("#nama").val("");
-
-  });
