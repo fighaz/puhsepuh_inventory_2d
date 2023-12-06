868d7d8e02f266686249a2d58715d4ba73a4149a
MohammadAlimul <mohammad4limul@gmail.com>
2023-12-06 09:02:27 +0700

    Menggunakan DataTables untuk list data

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -5,10 +5,14 @@
     <title></title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="js/jquery.js"></script>
+    <script src="js/datatables.min.js"></script>
+    <script src="js/dataTables.bootstrap5.min.js"></script>
+    <link href="css/datatables.min.css" rel="stylesheet">
+    <link href="css/dataTables.bootstrap5.min.css" rel="stylesheet">
     <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
     <!--<script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI/tZ1ZqjKw0BOyL8GfZ2mPAmUw/A763lSNtFqIo=" crossorigin="anonymous"></script>-->
-    <script src="js/jquery.js"></script>
 </head>
 
 <body>
