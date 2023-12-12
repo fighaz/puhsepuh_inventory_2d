3064c35c247f07a9996a18a98069c815027a29a1
MohammadAlimul <mohammad4limul@gmail.com>
2023-12-12 09:02:08 +0700

    Fix font

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -10,6 +10,7 @@
     <link  href="<?=BASEURL?>/css/datatables.min.css" rel="stylesheet">
     <link  href="<?=BASEURL?>/css/bootstrap.css" rel="stylesheet">
     <link  href="<?=BASEURL?>/css/style.css" rel="stylesheet">
+    <link  href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
     <!--<script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI/tZ1ZqjKw0BOyL8GfZ2mPAmUw/A763lSNtFqIo=" crossorigin="anonymous"></script>-->
 </head>
 
