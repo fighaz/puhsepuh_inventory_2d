9796ea4557a68c44b0ad9a0f17435102281e01a0
Abdul Aziz <73788986+abdaziz-04@users.noreply.github.com>
2023-12-13 21:46:00 +0700

    sweet alert script ada di header tinggal panggil aja

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -11,8 +11,44 @@
     <link  href="<?=BASEURL?>/css/bootstrap.css" rel="stylesheet">
     <link  href="<?=BASEURL?>/css/style.css" rel="stylesheet">
     <link  href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
     <!--<script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI/tZ1ZqjKw0BOyL8GfZ2mPAmUw/A763lSNtFqIo=" crossorigin="anonymous"></script>-->
+    
+    <!-- Script sweetalert -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
+    <script type="text/javascript">
+    function berhasil() {
+    Swal.fire({
+            title: "Berhasil!",
+            text: "Peminjaman berhasil diajukan",
+            icon: "success",
+            button: true
+        });
+    }
+    function gagal() {
+    Swal.fire({
+            title: "Berhasil!",
+            text: "Peminjaman tidak berhasil",
+            icon: "error",
+            button: true
+        });
+    }
+
+    function confirmDel({
+        title: "Apakah Anda yakin untuk menghapus Data Barang berikut?",
+        showDenyButton: true,
+        icon: "question",
+        confirmButtonText: "Yakin",
+        denyButtonText: `Batal`
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire("Berhasil dihapus!", "", "success");
+        } else if (result.isDenied) {
+            Swal.fire("dibatalkan", "", "");
+        }
+        });
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    
