bb86dc315a8f2c49516617153b7548ba78a2ead7
Abdul Aziz <73788986+abdaziz-04@users.noreply.github.com>
2023-12-14 08:09:54 +0700

    Tambah sweet alert berhasil login

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -25,11 +25,20 @@
             button: true
         });
     }
+
     function gagal() {
     Swal.fire({
             title: "Berhasil!",
             text: "Peminjaman tidak berhasil",
             icon: "error",
+            button: true
+        });
+    }
+
+    function berhasilLogin() {
+    Swal.fire({
+            title: "Berhasil Login!",
+            icon: "success",
             button: true
         });
     }
@@ -53,7 +62,7 @@
         });
     }
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
 
 <body>
