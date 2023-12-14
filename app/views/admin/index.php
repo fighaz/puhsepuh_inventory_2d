67d939d123f45b78135c084709b68de4be37e4ba
Abdul Aziz <73788986+abdaziz-04@users.noreply.github.com>
2023-12-14 21:21:24 +0700

    Tambah flasher di home admin

--- app/views/admin/index.php
+++ app/views/admin/index.php
@@ -136,3 +136,18 @@
     </nav>
   </div>
 </div>
+
+<script>
+  <?php
+    if (isset($_SESSION['login_success']) && $_SESSION['login_success']) {
+        unset($_SESSION['login_success']); 
+    ?>
+        Swal.fire({
+            title: 'Login Berhasil',
+            text: 'Selamat Datang!',
+            icon: 'success',
+            timer: 2000,
+            timerProgressBar: true,
+        });
+    <?php } ?>
+</script>
